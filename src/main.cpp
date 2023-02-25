139b5be78dfb2a578a1bff25fc2432d267c7d26e
Marcus Sanatan <msanatan@gmail.com>
2023-02-25 01:34:31 -0400

    This time we add a 3D cube and move around

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,51 @@
 #include "raylib.h"
+#include "Player.h"
 
 int main()
 {
-    // Initialize Raylib window
-    const int screenWidth = 800;
-    const int screenHeight = 640;
-    InitWindow(screenWidth, screenHeight, "Moving Square");
+  // Initialize Raylib window
+  const int screenWidth = 1024;
+  const int screenHeight = 768;
+  InitWindow(screenWidth, screenHeight, "Moving Cube");
 
-    // Initialize square position and size
-    Rectangle square = {screenWidth / 2 - 20, screenHeight / 2 - 20, 40, 40};
+  // Initialize square position and size
+  Rectangle square = {screenWidth / 2 - 20, screenHeight / 2 - 20, 40, 40};
 
-    SetTargetFPS(60); // Set the target frame rate to 60 FPS
+  // Set up camera
+  Camera3D camera = {0};
+  camera.position = (Vector3){0.0f, 10.0f, 10.0f}; // Camera position
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};     // Camera looking at point
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};         // Camera up vector (rotation towards target)
+  camera.fovy = 45.0f;                             // Camera field-of-view Y
+  camera.projection = CAMERA_PERSPECTIVE;          // Camera mode type
 
-    // Main game loop
-    while (!WindowShouldClose())
-    {
-        // Update square position based on arrow key input
-        if (IsKeyDown(KEY_LEFT))
-            square.x -= 5;
-        if (IsKeyDown(KEY_RIGHT))
-            square.x += 5;
-        if (IsKeyDown(KEY_UP))
-            square.y -= 5;
-        if (IsKeyDown(KEY_DOWN))
-            square.y += 5;
+  float speed = 0.0f;
+  Player player = Player(0.0f, 0.0f, 0.0f, 2.0f); // Create a player object
+  SetTargetFPS(60);                               // Set the target frame rate to 60 FPS
 
-        // Draw square
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawRectangleRec(square, MAROON);
-        EndDrawing();
-    }
+  // Main game loop
+  while (!WindowShouldClose())
+  {
+    // Update square position based on arrow key input
+    speed = GetFrameTime() * 5;
+    if (IsKeyDown(KEY_LEFT))
+      player.x -= speed;
+    if (IsKeyDown(KEY_RIGHT))
+      player.x += speed;
+    if (IsKeyDown(KEY_UP))
+      player.z -= speed;
+    if (IsKeyDown(KEY_DOWN))
+      player.z += speed;
 
-    // Close Raylib window and clean up resources
-    CloseWindow();
-    return 0;
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    BeginMode3D(camera);
+    player.draw();
+    EndMode3D();
+    EndDrawing();
+  }
+
+  // Close Raylib window and clean up resources
+  CloseWindow();
+  return 0;
 }
